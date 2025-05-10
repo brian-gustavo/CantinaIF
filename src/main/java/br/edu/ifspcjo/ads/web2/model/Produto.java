8b0c3b5557fc41710937bf4f8a18f28cc2a3c92a
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-05-09 22:34:47 -0300

    Update Produto.java

--- src/main/java/br/edu/ifspcjo/ads/web2/model/Produto.java
+++ src/main/java/br/edu/ifspcjo/ads/web2/model/Produto.java
@@ -1,1 +1,40 @@
+package model;
 
+public class Produto {
+	private String nome;
+	private String descricao;
+	private float preco;
+	private boolean disponibilidade;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public float getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+	
+	public boolean isDisponibilidade() {
+		return disponibilidade;
+	}
+	
+	public void setDisponibilidade(boolean disponibilidade) {
+		this.disponibilidade = disponibilidade;
+	}
+}
