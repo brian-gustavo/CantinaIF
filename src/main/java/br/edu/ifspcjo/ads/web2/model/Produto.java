7b772f97310a06882c4333f95dd1f016babe3bba
Tiago Jarruge Saraiva <163585218+TiagoJSaraiva@users.noreply.github.com>
2025-05-16 22:24:20 -0300

    Update Produto.java
    Adicionei o objeto ID, que estava faltando e é a primary key da entidade.

--- src/main/java/br/edu/ifspcjo/ads/web2/model/Produto.java
+++ src/main/java/br/edu/ifspcjo/ads/web2/model/Produto.java
@@ -1,47 +1,56 @@
 package model;
 
 public class Produto {
-	private String nome;
-	private String descricao;
-	private float preco;
-	private int estoque;
+    private int id;
+    private String nome;
+    private String descricao;
+    private float preco;
+    private int estoque;
 	
-	public Produto(String nome, String descricao, float preco, int estoque) {
+    public Produto(String nome, String descricao, float preco, int estoque) {
         this.nome = nome;
         this.descricao = descricao;
         this.preco = preco;
         this.estoque = estoque;
     }
-	
-	public String getNome() {
-		return nome;
-	}
-	
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public String getDescricao() {
-		return descricao;
-	}
-	
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	
-	public float getPreco() {
-		return preco;
-	}
-	
-	public void setPreco(float preco) {
-		this.preco = preco;
-	}
-	
-	public int getEstoque() {
-		return estoque;
-	}
-	
-	public void setEstoque(int estoque) {
-		this.estoque = estoque;
-	}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
 }
