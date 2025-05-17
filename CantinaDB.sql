5a81778c712af5485ac84f4149474bbf3dab9ab6
Tiago Jarruge Saraiva <163585218+TiagoJSaraiva@users.noreply.github.com>
2025-05-16 22:03:56 -0300

    Update CantinaDB.sql
    Atualização SQL

--- CantinaDB.sql
+++ CantinaDB.sql
@@ -1,12 +1,41 @@
 CREATE DATABASE todoapp CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
 USE todoapp;
 
-CREATE TABLE usuarios (
+CREATE TABLE Comprador (
     prontuario VARCHAR(10) PRIMARY KEY,
     email VARCHAR(100) UNIQUE,
     senha VARCHAR(100),
     nome VARCHAR(75)
+);
+
+CREATE TABLE Vendedor (
+    prontuario VARCHAR(10) PRIMARY KEY,
+    email VARCHAR(100) UNIQUE,
+    senha VARCHAR(100)
+);
+
+CREATE TABLE Produto (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+    descricao VARCHAR(400),
+    preco DECIMAL(10,2),
+    estoque INT
+);
+
+CREATE TABLE Pedido (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    comprador_prontuario VARCHAR(10),
+    FOREIGN KEY (comprador_prontuario) REFERENCES Comprador(prontuario)
+);
+
+-- Tabela intermediária para associar pedidos e produtos
+CREATE TABLE Pedido_Produto (
+    pedido_id INT,
+    produto_id INT,
+    quantidade INT DEFAULT 1,
+    PRIMARY KEY (pedido_id, produto_id),
+    FOREIGN KEY (pedido_id) REFERENCES Pedido(id),
+    FOREIGN KEY (produto_id) REFERENCES Produto(id)
 );
 
 SELECT * FROM usuarios;
