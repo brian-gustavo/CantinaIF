6339f9a508cdb329d410cf391efd70d3e25c883b
Tiago Jarruge Saraiva <163585218+TiagoJSaraiva@users.noreply.github.com>
2025-05-19 21:42:14 -0300

    Update CantinaDB.sql
    Atualização da entidade produto (adição da chave estrangeira que relaciona o produto com seu respectivo vendedor)

--- CantinaDB.sql
+++ CantinaDB.sql
@@ -23,7 +23,9 @@ CREATE TABLE Produto (
     descricao VARCHAR(400),
     preco DECIMAL(10,2),
     estoque INT,
-    categoria ENUM('salgado', 'doce', 'lanche', 'bebida')
+    categoria ENUM('salgado', 'doce', 'lanche', 'bebida'),
+    vendedor_prontuario VARCHAR(10),
+    FOREIGN KEY (vendedor_prontuario) REFERENCES Vendedor(prontuario)
 );
 
 -- Tabela do vendedor (O usuário que insere os pedidos no sistema)
