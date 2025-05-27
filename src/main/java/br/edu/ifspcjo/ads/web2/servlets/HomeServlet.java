10ee47ce4028c739135d15e577b9d504fa5ee1d1
Luiz Henrique Freitas <166095630+LuizHenriqueFreitas@users.noreply.github.com>
2025-05-26 22:30:51 -0300

    Update HomeServlet.java

--- src/main/java/br/edu/ifspcjo/ads/web2/servlets/HomeServlet.java
+++ src/main/java/br/edu/ifspcjo/ads/web2/servlets/HomeServlet.java
@@ -1,8 +1,7 @@
-package servlets;
+package servlet;
 
-import dao.ProductDao;
+import Dao.ProductDao;
 import model.Produto;
-import model.Comprador;
 import utils.ConnectionFactory;
 
 import jakarta.servlet.ServletException;
@@ -10,29 +9,21 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.util.List;
 
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-    	HttpSession session = request.getSession(false);
 
-        if (session != null && session.getAttribute("comprador") != null) {
-            Comprador comprador = (Comprador) session.getAttribute("comprador");
-            request.setAttribute("mensagemBoasVindas", "Olá, " + comprador.getNome() + ", bem-vindo ao seu painel!");
-            request.getRequestDispatcher("registroProdutos.jsp").forward(request, response);
-        } else {
-            response.sendRedirect("login.jsp");
-        }
-        
         try (Connection connection = ConnectionFactory.getConnection()) {
             ProductDao productDao = new ProductDao(connection);
-            List<Produto> produtos = productDao.listarProdutos();
+            List<Produto> produtos = productDao.listarTodos();
 
             // Envia a lista para a página JSP
             request.setAttribute("produtos", produtos);
