dd6fff2b61ff067c36cef3e43b7344248fda9e22
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-05-30 19:32:04 -0300

    Update Comprador.java

--- src/main/java/model/Comprador.java
+++ src/main/java/model/Comprador.java
@@ -7,10 +7,10 @@ public class Comprador {
 	private String nome;
 	
 	public Comprador(String prontuario, String email, String senha, String nome) {
-        this.prontuario = prontuario;
-        this.email = email;
-        this.senha = senha;
-        this.nome = nome;
+	        this.prontuario = prontuario;
+	        this.email = email;
+	        this.senha = senha;
+	        this.nome = nome;
     	}
 	
 	public String getProntuario() {
