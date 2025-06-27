daefea96f194802afa086cce9ab04cd8683b4a4c
Brian Gustavo de Oliveira Brandão <brian20062005@gmail.com>
2025-06-27 14:35:12 -0300

    Update Pedido.java

--- src/main/java/model/Pedido.java
+++ src/main/java/model/Pedido.java
@@ -1,37 +1,52 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedido {
-	private int id;
-	private Comprador comprador;
-	private Produto produto;
-	
-	public Pedido(int id, Comprador comprador, Produto produto) {
-	        this.id = id;
-	        this.comprador = comprador;
-	        this.produto = produto;
-    	}
-	
-	public int getId() {
-		return id;
-	}
-	
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	public Comprador getComprador() {
-		return comprador;
-	}
-	
-	public void setComprador(Comprador comprador) {
-		this.comprador = comprador;
-	}
-	
-	public Produto getProduto() {
-		return produto;
-	}
-	
-	public void setProduto(Produto produto) {
-		this.produto = produto;
-	}
+    private int id;
+    private Comprador comprador;
+    private List<ItemCarrinho> itens;
+
+    public Pedido() {
+        this.itens = new ArrayList<>();
+    }
+
+    public Pedido(int id, Comprador comprador) {
+        this.id = id;
+        this.comprador = comprador;
+        this.itens = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Comprador getComprador() {
+        return comprador;
+    }
+
+    public List<ItemCarrinho> getItens() {
+        return itens;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setComprador(Comprador comprador) {
+        this.comprador = comprador;
+    }
+
+    public void setItens(List<ItemCarrinho> itens) {
+        this.itens = itens;
+    }
+
+    public void addItem(ItemCarrinho item) {
+        if (this.itens == null) {
+            this.itens = new ArrayList<>();
+        }
+        this.itens.add(item);
+    }
 }
